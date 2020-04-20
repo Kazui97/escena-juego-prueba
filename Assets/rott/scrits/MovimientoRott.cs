51d2af85cfdef0dc9ac06480044e382a3a56e149
Kazui97 <camyescobar12@gmail.com>
2020-04-20 18:38:35 -0500

    Initial commit

---
+++ Assets/rott/scrits/MovimientoRott.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoRott : MonoBehaviour
+{
+
+    private Animator Animaciones;
+    public float Velocidad;
+    public float Rotacion;
+    public float x, y;
+
+
+    private void Awake()
+    {
+        Animaciones = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Rotacion * Time.deltaTime, 0);
+        transform.Translate(0, 0, y * Velocidad * Time.deltaTime);
+
+        Animaciones.SetFloat("VelX", x);
+        Animaciones.SetFloat("VelY", y);
+    }
+}
